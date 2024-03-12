0323d517711488b4348429a33bfc050c97d01c02
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-12 17:06:57 +0530

    updated usermodel ny ksdev

---
+++ backend/models/UserModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserModel = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+});
+
+const user = mongoose.model("users", UserModel);
+
+module.exports = user;
